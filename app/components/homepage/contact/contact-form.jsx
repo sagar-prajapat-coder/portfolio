c730723d0c7446842d8f1381528ca0584935f825
Sagar Prajapat <109717363+sagar-prajapat-coder@users.noreply.github.com>
2025-07-16 12:09:42 +0530

    Update contact-form.jsx

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -36,7 +36,7 @@ function ContactForm() {
     try {
       setIsLoading(true);
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/v1/contact`,
+        `${process.env.NEXT_PUBLIC_APP_URL}api/v1/contact`,
         userInput
       );
 
