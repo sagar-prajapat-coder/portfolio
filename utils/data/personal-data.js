733a99d5e98e8fcf7f81318705b55b3a23557684
sagar prajapat <sagarprajapat250@gmail.com>
2025-12-27 15:06:27 +0530

    adding typing efferct

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,6 +2,11 @@ export const personalData = {
   name: "SAGAR PRAJAPATI",
   profile: '/profile.jpg',
   designation: "Software Developer",
+  roles: [
+    "Software Engineer",
+    "Full-Stack Developer",
+    "Back-end Developer",
+  ],
   description: "My name is SAGAR PRAJAPAT. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'sagarprajapat250@gmail.com',
   phone: '+917378159185',
